e98246edcfa167f822d84bee7cd5dc19d424f1cc
Rascal0910 <c0r11007@bunkyo.ac.jp>
2022-09-10 18:31:05 +0900

    [add]記事詳細画面

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,13 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use app\Category;
 use App\Post;
+use App\Http\Request\PostRequest;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-   public function index(Post $post)
-   {
-     return 'aaa';
-   }
+public function index(Post $post)
+{
+    return view('index')->with(['posts' => $post->getPaginateByLimit()]);
+} 
+    //上記　posts indexでは開かなかった。カリキュラムとは違うことに気をつける。
+
+public function show(Post $post)
+{
+    return view('show')->with(['posts' => $post]);
 }
+}
+?>
