98eed9879632eda36077656e774cefe40d986d17
Rascal0910 <c0r11007@bunkyo.ac.jp>
2022-09-14 18:21:10 +0900

    8-5

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -30,4 +30,14 @@ class PostController extends Controller
         $post->fill($input)->save();
         return redirect('/posts/' . $post->id);
     }
+    public function edit(Post $post)
+{
+    return view('posts/edit')->with(['posts' => $post]);
+}
+public function update(PostRequest $request, Post $post)
+{
+    $input_post = $request['post'];
+    $post->fill($input_post)->save();
+    return redirect('/posts/' . $post->id);
+}
 }
